dd0bb6814dbce809eeeb22738be69245ffc5d93f
Esuck09 <qikai.tan@gmail.com>
2023-05-11 09:08:30 +0100

    initial commit

--- InceptionV3.m
+++ InceptionV3.m
@@ -51,31 +51,3 @@ options = trainingOptions('sgdm', ...
 %%
 clc
 TrainedInception = trainNetwork(augimdsTrain,myNet,options);
-
-
-%%
-[YPred,scores] = classify(netTransfer,augimdsValidation)
-
-%%
-YValidation = imdsValidation.Labels;
-accuracy = mean(YPred == YValidation)
-
-
-%%
-cm = confusionmat(YValidation,YPred);
-cmt = cm'
-
-%% calculation for F1 score
-diagonal = diag(cmt)
-sum_of_rows = sum(cmt, 2)
-
-precision = diagonal ./sum_of_rows
-avg_precision = mean(precision)
-
-
-sum_of_columns = sum(cmt, 1)
-
-recall = diagonal ./ sum_of_columns'
-avg_recall = mean(recall)
-
-f1_score = 2*((avg_precision*avg_recall) / (avg_precision+avg_recall))
